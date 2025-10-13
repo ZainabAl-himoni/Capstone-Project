e75ace0c3fc6e9a87ed2e290ec55264926eb1ee9
unknown <zainabalhimoni19@gmail.com>
2025-10-13 11:49:07 +0300

    templates and static

--- lms/lms_app/views.py
+++ lms/lms_app/views.py
@@ -1,20 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Category
 
+
 def home(request):
+    
     books = Book.objects.all()
 
-    
     categories = Category.objects.prefetch_related('book_set').all()
 
-    
-    chart_labels = [cat.name for cat in categories]
-    chart_data = [cat.book_set.count() for cat in categories]
+    chart_labels = [category.name for category in categories]
+    chart_data = [category.book_set.count() for category in categories]
 
     context = {
         'books': books,
         'categories': categories,
         'chart_labels': chart_labels,
-        'chart_data': chart_data
+        'chart_data': chart_data,
     }
+
     return render(request, 'home.html', context)
+
+
+def about(request):
+    
+    return render(request, 'about.html')
+
+
+def admin_redirect(request):
+    
+    return redirect('/admin/')
